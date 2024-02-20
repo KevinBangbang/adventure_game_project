683b95f42d62b250744fed93de350d5e7b455ca9
KevinBangbang <129433941+KevinBangbang@users.noreply.github.com>
2024-02-20 12:06:53 -0500

    Add files via upload

---
+++ GameTests.java
@@ -0,0 +1,18 @@
+//import AdventureModel.AdventureGame;
+//import AdventureModel.npc.VisitableNPC;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//public class GameTests {
+//
+//    @Test
+//    void GetNPCFromRoomTest() {
+//        AdventureGame game = new AdventureGame("TinyGame");
+//        VisitableNPC npc = game.getNPCInitializer().getNPCFromRoomNumber(1);
+//        assertTrue(npc != null);
+//
+//        //game.interpretAction("TALK");
+//        game.player.visitNPC(npc);
+//        assertTrue(game.player.getInteractingNPC() != null);
+//    }
+//}
